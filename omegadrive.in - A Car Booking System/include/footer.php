77c96d7aabc1a80d6e08082ae35a452f402de360
deskOfVirendra <79376382+deskOfVirendra@users.noreply.github.com>
2021-07-28 23:34:03 +0530

    created footer.php

---
+++ omegadrive.in - A Car Booking System/include/footer.php
@@ -0,0 +1,39 @@
+
+<footer class="py-sm-5 py-4 px-md-5 px-3">
+	<div class="container-fluid pt-lg-5">
+		<div class="row">
+
+			<div class="col-lg-3 col-sm-6 mb-lg-0 mb-5 footer-grid-agileits-w3ls1 text-left">
+				<h3 class="mb-sm-5 mb-4">Locate Us</h3>
+				<ul class="w3ls-footer-bottom-list">
+					<li> <span class="fas fa-map-marker"></span> Flat No 409, Building No. 7,Anand Dham Society, Near Tulsidham, Pokhran Road No 2,Thane West,Thane,Maharashtra </li>
+					<li> <span class="fas fa-envelope"></span> <a href="mailto:name@example.com"> mail@example.com</a> </li>
+					<li> <span class="fas fa-phone"></span> +91 9096139047</li>
+					
+					<li> <span class="fas fa-globe"></span> <a href="https://www.omegadrive.in"> www.omegadrive.in</a> </li>
+					<li> <span class="fas fa-clock"></span> Office Time  : 8:00 a.m - 6:00 p.m</li>
+				</ul>
+			</div>
+
+			<!-- subscribe -->
+
+				<!-- //subscribe -->
+
+
+
+
+
+
+			</div>
+		</div>
+		<!-- copyright -->
+	</footer>
+
+	<div class="footer-cpy text-center pt-sm-5 mt-sm-5 mt-4 pt-3">
+		<div class="w3layouts-agile-copyrightbottom">
+			<p>© 2020 Omega Drive . All Rights Reserved | Developed By VOID</p>
+
+		</div>
+	</div>
+		<!-- //copyright -->
+	</div>
